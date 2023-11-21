479abeb78c795c1ff88c32e48aca351ef5803684
Salama Ashoush <salamaashoush@gmail.com>
2023-11-21 15:17:39 +0100

    Refactor utility ts guards

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,7 +1,7 @@
 export { default as memoize } from 'fast-memoize';
-export { default as camelCase } from 'just-camel-case';
 export { default as get } from 'just-safe-get';
 export { default as set } from 'just-safe-set';
+export * from 'scule';
 
 export * from './array';
 export * from './clone';
